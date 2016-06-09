cc8bfef35521e829b922111ec2aca58b30f74809
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-06-09 13:34:25 +0900

    Create sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'help'=> 'static_pages#help'
   get 'about'=> 'static_pages#about'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 end
