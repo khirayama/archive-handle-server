73aaf681d3584f0906c07b17bb7213aee3d00d7a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-06-20 22:18:21 +0900

    Fix typos

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,11 +11,12 @@ module SessionsHelper
   end
 
   def current_user
-    if (user_id = session[:user_id]) # user_idはどっからでてきたんだよ...
+    # user_idはどっからでてきたんだよ...
+    if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
-    elsif (user_id = cookies.signed[:user_id)
+    elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token)
+      if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
