4b04ddc115bfba7dc37c99bc0a43bca7ddd65664
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-07-10 10:08:46 +0900

    Implement events#new and events#create

--- latest/app/models/event.rb
+++ latest/app/models/event.rb
@@ -1,9 +1,9 @@
 class Event < ActiveRecord::Base
-  validate :name, length: { maximum: 50 }, presence: true
-  validate :place, length: { maximum: 100 }, presence: true
-  validate :content, length: { maximum: 2000 }, presence: true
-  validate :start_time, presence: true
-  validate :end_time, presence: true
+  validates :name, length: { maximum: 50 }, presence: true
+  validates :place, length: { maximum: 100 }, presence: true
+  validates :content, length: { maximum: 2000 }, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
   validate :start_time_should_be_before_end_time
 
   private
