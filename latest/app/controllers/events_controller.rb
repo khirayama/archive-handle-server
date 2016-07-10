216ff96894865eb229bbaa0ba1a7f7d8584c08bc
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-07-10 09:57:52 +0900

    Implement event model

--- latest/app/controllers/events_controller.rb
+++ latest/app/controllers/events_controller.rb
@@ -1,2 +1,24 @@
 class EventsController < ApplicationController
+  before_action :authenticate
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_parmas)
+    if @event.save
+      redirect_to @event, notice: 'Created event'
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def event_parmas
+      params.require(:event).permit(
+        :name, :place, :content, :start_time, :end_time
+      )
+    end
 end
